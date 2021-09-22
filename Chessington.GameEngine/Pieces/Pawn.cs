351877ace01c4f8a4c41d7268de3d23b7608a42f
RhysSte <R.Stedman@ucas.ac.uk>
2021-09-22 16:24:54 +0100

    made prawns move

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,33 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            
+            
+
+            // find current location
+            Square currentPosition = board.FindPiece(this);
+
+            // find square one up from it
+            Square newPosition;
+
+            if (Player == Player.White)
+            {
+                newPosition = Square.At(currentPosition.Row - 1, currentPosition.Col);
+            }
+            else
+            {
+                newPosition = Square.At(currentPosition.Row + 1, currentPosition.Col);
+            }
+
+
+            // board.findpiece
+
+            //put square in list
+
+            var moves = new List<Square>();
+            moves.Add(newPosition);
+            return moves;
+
         }
     }
 }
